efdc582828f5db8030752f024214df9fa9842d5b
Jean-Marc Weeger <jmweeger@ugc.fr>
2025-09-20 15:50:00 +0200

    feat(plan): Add check box for completed tasks

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 
 import 'package:flutter/material.dart';
 
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'l10n/app_localizations.dart';
 
 void main() {
---
+++ test/main_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:remind_me/main.dart' as app;
+
+void main() {
+  test('main() runs without error', () {
+    app.main();
+  });
+}
